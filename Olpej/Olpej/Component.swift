578b1ab250ed4b9c9d1d3e55aa01bff8c182b1a4
Luke Matthew Sutton <me@lukematthewsutton.com>
2016-08-15 17:55:20 +0930

    Make properties into a Style struct

--- Olpej/Olpej/Component.swift
+++ Olpej/Olpej/Component.swift
@@ -25,51 +25,52 @@ public func ==<A>(lhs: ComponentIdentifier<A>, rhs: ComponentIdentifier<A>) -> B
 }
 
 public protocol ComponentType {
-    func render(to targetView: UIView?) -> UIView
-    var hashValue: Int { get }
+    func render(to targetView: UIView?, horizontal: UIUserInterfaceSizeClass, vertical: UIUserInterfaceSizeClass, direction: UIUserInterfaceLayoutDirection) -> UIView
 }
 
-public struct Component<View: UIView>: ComponentType, Equatable {
+public struct Component<View: UIView>: ComponentType {
     public let identifier: ComponentIdentifier<View>
     public let styles: [Style<View>]
-    public let directProperties: Set<Property<View>>
-    public let properties: Set<Property<View>>
     public let children: [ComponentType]
 
     public init(id: ComponentIdentifier<View>, styles: [Style<View>] = [], properties: [Property<View>] = [], children: [ComponentType] = []) {
         self.identifier = id
-        self.styles = styles
-        self.directProperties = Set(properties)
-        self.properties = Set(styles.flatMap {$0.properties} + properties)
         self.children = children
+
+        if !properties.isEmpty {
+            self.styles = styles + [Style(properties: properties)]
+        }
+        else {
+            self.styles = styles
+        }
     }
 
     public init(id: String, styles: [Style<View>] = [], properties: [Property<View>] = [], children: [ComponentType] = []) {
         self.init(id: .id(id), styles: styles, properties: properties, children: children)
     }
-    
-    public var hashValue: Int {
-        return properties.reduce(0) {$0 &+ $1.hashValue} &+ children.reduce(0) {$0 &+ $1.hashValue}
-    }
 }
 
-public func ==<A, B>(lhs: Component<A>, rhs: Component<B>) -> Bool {
-    return false
-}
-
-public func ==<A>(lhs: Component<A>, rhs: Component<A>) -> Bool {
-    return lhs.hashValue == rhs.hashValue
+private struct IntermediateComponent<View: UIView> {
+    let identifier: ComponentIdentifier<View>
+    let properties: Set<Property<View>>
 }
 
 extension Component {
-    public func render(to targetView: UIView? = nil) -> UIView {
+    private func intermediateFor(horizontal horizontal: UIUserInterfaceSizeClass, vertical: UIUserInterfaceSizeClass, direction: UIUserInterfaceLayoutDirection) -> IntermediateComponent<View> {
+        let applicableStyles = styles.filter {$0.appliesTo(horizontal: horizontal, vertical: vertical, direction: direction)}
+        let properties = applicableStyles.flatMap {$0.properties}
+        return IntermediateComponent(identifier: identifier, properties: Set(properties))
+    }
+
+    public func render(to targetView: UIView? = nil, horizontal: UIUserInterfaceSizeClass = .Unspecified, vertical: UIUserInterfaceSizeClass = .Unspecified, direction: UIUserInterfaceLayoutDirection = .LeftToRight) -> UIView {
         let view = View()
         targetView?.op_addSubview(view)
-        for prop in properties {
+        let intermediate = intermediateFor(horizontal: horizontal, vertical: vertical, direction: direction)
+        for prop in intermediate.properties {
             prop.update(identifier, .update, view)
         }
         for child in children {
-            child.render(to: view)
+            child.render(to: view, horizontal: horizontal, vertical: vertical, direction: direction)
         }
         return view
     }
--- Olpej/Olpej/Style.swift
+++ Olpej/Olpej/Style.swift
@@ -1,6 +1,51 @@
 import Foundation
 
+public enum StyleSize: Int {
+    case any
+    case compact
+    case regular
+}
+
+public enum StyleLayoutDirection: Int {
+    case any
+    case leftToRight
+    case rightToLeft
+}
+
 public struct Style<V: UIView> {
-    let label: String
+    let horizontal: StyleSize
+    let vertical: StyleSize
+    let direction: StyleLayoutDirection
     let properties: [Property<V>]
+    let description: String?
+
+    init(horizontal: StyleSize = .any, vertical: StyleSize = .any, direction: StyleLayoutDirection = .any,  properties: [Property<V>], description: String? = nil) {
+        self.horizontal = horizontal
+        self.vertical = vertical
+        self.direction = direction
+        self.properties = properties
+        self.description = description
+    }
+}
+
+extension Style {
+    func appliesTo(horizontal horizontal: UIUserInterfaceSizeClass, vertical: UIUserInterfaceSizeClass, direction: UIUserInterfaceLayoutDirection) -> Bool {
+        return (self.horizontal == .any || self.horizontal.rawValue == horizontal.rawValue) &&
+               (self.vertical == .any || self.vertical.rawValue == vertical.rawValue) &&
+               (self.direction == .any || self.direction.rawValue == direction.rawValue)
+    }
+}
+
+extension Style: Hashable {
+    public var hashValue: Int {
+        return properties.reduce(0) {$0 + $1.hashValue} + horizontal.hashValue + vertical.hashValue + direction.hashValue
+    }
+}
+
+public func == <A, B>(lhs: Style<A>, rhs: Style<B>) -> Bool {
+    return false
+}
+
+public func == <A>(lhs: Style<A>, rhs: Style<A>) -> Bool {
+    return lhs.hashValue == rhs.hashValue
 }
